d5d35eded3eb166b4478bf40b14500864a0637be
Lavanya Ranganathan <M1048373@mindtree.com>
2019-11-13 15:34:26 +0530

    first commit

---
+++ src/main/java/SampleRestAssuredBasicProj/SampleRestAssuredBasicProj/App.java
@@ -0,0 +1,13 @@
+package SampleRestAssuredBasicProj.SampleRestAssuredBasicProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
